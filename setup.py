6ccbe1f0b28b1ff308315e88e9dfbeaebed7ae2c
Marcin Sztolcman <marcin@urzenia.net>
2021-03-17 23:21:00 +0100

    0.4.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as f:
 
 setup(
     name='smtpc',
-    version='0.4.0',
+    version='0.4.1',
     description='',
     long_description=readme,
     long_description_content_type='text/markdown',
