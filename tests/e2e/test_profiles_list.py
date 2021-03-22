80e4345314325c01dab51c8d41d98fe890d3062a
Marcin Sztolcman <marcin@urzenia.net>
2021-03-22 19:24:35 +0100

    tests improvemnts

--- tests/e2e/test_profiles_list.py
+++ tests/e2e/test_profiles_list.py
@@ -10,7 +10,7 @@ def test_list_empty(smtpctmppath, capsys):
     r = callsmtpc(['profiles', 'list'], capsys)
 
     assert r.code == ExitCodes.OK.value
-    assert 'No known profiles\n' == r.out
+    assert 'No known profiles\n' == r.out, 'Profiles list not empty?'
 
 
 def test_list_profiles(smtpctmppath, capsys):
@@ -19,8 +19,8 @@ def test_list_profiles(smtpctmppath, capsys):
     assert r.code == ExitCodes.OK.value
     data = load_toml_file(smtpctmppath / config.PREDEFINED_PROFILES_FILE.name)
     profiles = data['profiles']
-    assert 'simple1' in profiles
-    assert profiles['simple1'] == {'host': 'localhost', 'login': 'asd', 'password': 'qwe'}
+    assert 'simple1' in profiles, 'Expected profile simple1 not found'
+    assert profiles['simple1'] == {'host': 'localhost', 'login': 'asd', 'password': 'qwe'}, 'Expected profile simple1 is invalid'
 
     r = callsmtpc(['profiles', 'list'], capsys)
     assert 'Known profiles:\n- simple1\n' == r.out
